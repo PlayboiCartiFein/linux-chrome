e9bea3409a4c8eab58fd0b29aa177a0b22944667
PlayboiCartiFein <someonenamededgar@outlook.com>
2025-09-27 00:07:42 -0400

    Add headless support for no-screen environments

--- example.js
+++ example.js
@@ -1,6 +1,14 @@
 const chrome = require('.')
 
+// Example 1: Regular Chrome with GUI
 const ps = chrome({
   uri: 'https://github.com/'
 })
 ps.on('error', console.error)
+
+// Example 2: Headless Chrome (no screen needed)
+const headlessPs = chrome({
+  uri: 'https://github.com/',
+  headless: true
+})
+headlessPs.on('error', console.error)
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const spawn = require('child_process').spawn
 
 module.exports = (opts, cb) => {
+  opts = opts || {}
   const env = Object.assign({}, process.env)
   if (opts.display) env.DISPLAY = `:${opts.display}`
 
   const dataDir = opts.dataDir ||
     `/tmp/${Math.random().toString(16).slice(2)}`
   const args = [
+    opts.headless && '--headless',
+    opts.headless && '--disable-gpu',
+    opts.headless && '--no-sandbox',
     opts.proxy && `--proxy-server=${opts.proxy}`,
     `--user-data-dir=${dataDir}`,
     '--disable-restore-session-state',
     '--no-default-browser-check',
-    '--start-maximized',
+    !opts.headless && '--start-maximized',
     '--disable-default-apps',
     '--disable-sync',
     '--enable-fixed-layout',
